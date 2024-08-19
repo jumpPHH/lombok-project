f02905719ecae846e4a85b5584a0e95e26d5723a
Han Hee Park <hbag88668@gmail.com>
2024-08-19 15:34:15 +0900

    'empty project first commit'

--- src/main/java/com/lombok/lombok_project/LombokProjectApplication.java
+++ src/main/java/com/lombok/lombok_project/LombokProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LombokProjectApplication {
-
+	// empty project first commit
 	public static void main(String[] args) {
 		SpringApplication.run(LombokProjectApplication.class, args);
 	}
