f7df123a5c6157e9e3971ecd11d6f657deff8a45
Han Hee Park <hbag88668@gmail.com>
2024-08-19 14:28:19 +0900

    Create a Lombuk project repository

---
+++ src/main/java/com/lombok/lombok_project/LombokProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lombok.lombok_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokProjectApplication.class, args);
+	}
+
+}
